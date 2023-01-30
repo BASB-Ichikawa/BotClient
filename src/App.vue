77835f0fefb35303eede084a8e701863dcd5577b
r.ichikawa@basb.jp <r.ichikawa@basb.jp>
2023-01-30 15:14:31 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,103 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import * as SpeechSDK from "microsoft-cognitiveservices-speech-sdk";
+// import * as axios from 'axios';
+
+async function onStartRecording() {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: false
+  })
+
+  const recorder = new MediaRecorder(stream, {
+    mimeType: 'video/webm;codecs=vp9'
+  });
+
+  const chunks = <any>[];  
+  recorder.addEventListener('dataavailable', function (ele) {
+    if (ele.data.size > 0) {
+      chunks.push(ele.data);
+    }
+  });
+
+  recorder.addEventListener('stop', function() {
+    console.log(chunks)
+  });
+};
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <div class="wrapper">
+      Speach To Text検証
+      <button @click="onStartRecording">開始</button>
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
