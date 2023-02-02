b2eeda559fff387e9e1f128adf2a798d156c4024
r.ichikawa@basb.jp <r.ichikawa@basb.jp>
2023-02-02 16:45:42 +0900

    0202

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,12 +6,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'top',
+      path: '/login',
+      name: 'login',
       component: LoginView
     },    
     {
-      path: '/top',
+      path: '/',
       name: 'top',
       component: TopView
     },  
