1d1096bffcd76622cdf5b93b4ac8a873bbab8a05
r.ichikawa@basb.jp <r.ichikawa@basb.jp>
2023-02-20 15:11:12 +0900

    0220

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,9 +5,12 @@ import { PublicClientApplication } from "@azure/msal-browser"
 
 const config = {
   auth: {
-    clientId: '02a46cb4-3c1e-46a0-b2c3-e8c5e6ac14b9', // SPA側のクライアントID
-    authority: 'https://login.microsoftonline.com/16b3c013-d300-468d-ac64-7eda0820b6d3',
+    //clientId: '02a46cb4-3c1e-46a0-b2c3-e8c5e6ac14b9', // SPA側のクライアントID
+    //authority: 'https://login.microsoftonline.com/16b3c013-d300-468d-ac64-7eda0820b6d3',
     //authority: 'https://login.microsoftonline.com/organizations', // マルチテナント用の設定 (但し、MSが公式に許可しているサービス(GrapAPIなど)のAPIに限る)
+
+    clientId: 'c982c769-156c-43a2-a976-38fd49b500f8', // SPA側のクライアントID
+    authority: 'https://login.microsoftonline.com/c138f920-1c56-4cfa-b535-c025b2aedb44',
     redirectUri: import.meta.env.VITE_WEB_URL
   },
   cache: {
@@ -20,20 +23,23 @@ const router = useRouter();
 let app: PublicClientApplication = new PublicClientApplication(config)
 
 onMounted(async () => {
-  app.handleRedirectPromise().then(async (response: any) => {
-    if (response !== null) {
-      if (app.getAllAccounts().length > 0) {
-        app.setActiveAccount(app.getAllAccounts()[0]);
-        const result = await app.acquireTokenSilent({
-          // APIを公開した時に発行されるスコープ値 (SPA側に事前にアクセス許可を追加しておく必要あり)
-          scopes: ['api://c527d880-7610-4fd4-918e-bbbda5d7a2b2/WebApiUsingDirectLine'],
-        })
-        
-        localStorage.setItem('token', result.accessToken)        
-        router.push('/top')
-      }
+  const response = await app.handleRedirectPromise().catch(error => console.error({error}))
+  // IDPからのリダイレクト時のみresponseに値が含まている (つまり画面の初期表示時はnull)
+  if (response !== null) {
+    // 複数のIDPで認証した場合は複数のAccountが取得される
+    if (app.getAllAccounts().length > 0) {
+      app.setActiveAccount(app.getAllAccounts()[0]);
+      // アクセストークンを取得
+      const result = await app.acquireTokenSilent({
+        // APIを公開した時に発行されるスコープ値 (SPA側のSPに事前にアクセス許可を追加しておく必要あり)
+        //scopes: ['api://c527d880-7610-4fd4-918e-bbbda5d7a2b2/WebApiUsingDirectLine'],
+        scopes: ["User.Read"],
+      })
+      alert(result.accessToken)
+      router.push('/top')
     }
-  })
+  }
+
 })
 
 async function onClickLogin() {
@@ -42,8 +48,6 @@ async function onClickLogin() {
     const result = await app.acquireTokenSilent({
       scopes: ['User.Read'],
     })
-
-    localStorage.setItem('token', result.accessToken)
     router.push('/top')
   } else {
     // Redirect型ログイン
